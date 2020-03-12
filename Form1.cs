42e6dfa897e622697006c534bcd00103745a6355
Kris Clancy <49080445+KrisPC@users.noreply.github.com>
2020-03-11 20:18:03 -0500

    Comments

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿//Author:Kris, Noah, David
+// date 03/11/2020
+// objective: proof of concept 
+//API used alphavatage
+using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace StockProgram
 {
     public partial class Form1 : Form
@@ -10,23 +13,20 @@ namespace StockProgram
         {
             InitializeComponent();
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
         }
-
         private async void button1_Click(object sender, EventArgs e)
         {
+//take in a stock(s) and using the API find the stock and its
+// current value display this to the screen. 
             string input = textBox1.Text;
             Stock s = await Program.RunASync(input);
             textBox2.Text = "Name of stock: " + s.Name + Environment.NewLine + "Price of Stock: " + s.Price;
-
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
         }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
         }
--- Program.cs
+++ Program.cs
@@ -1,68 +1,68 @@
-﻿using Newtonsoft.Json.Linq;
+﻿//Author:Kris, Noah, David
+// date 03/11/2020
+// objective: proof of concept 
+//API used alphavatage
+using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// will be named later 
 namespace StockProgram
 {
-    internal class Stock
+  internal class Stock
+  {
+    public Double Price { get; set; }
+    public string Name { get; set; }
+  }
+  internal static class Program
+  {
+    [STAThread]
+    private static void Main()
     {
-        public Double Price { get; set; }
-        public string Name { get; set; }
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
     }
-
-    internal static class Program
+    public static async Task<Stock> RunASync(string input)
     {
-        [STAThread]
-        private static void Main()
+      using (var client = new HttpClient())
+      {
+        client.BaseAddress = new Uri("https://www.alphavantage.co/");
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        HttpResponseMessage response = await client.GetAsync($"query?function=GLOBAL_QUOTE&symbol={input}&apikey=1IB76KJ1VBK8IQXF");
+        try
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-
-        
-
-        public static async Task<Stock> RunASync(string input)
-        {
-            using (var client = new HttpClient())
+          if (response.IsSuccessStatusCode)
+          {
+            JObject jsonResponse = await response.Content.ReadAsAsync<JObject>();
+            Stock currStock = new Stock
             {
-                client.BaseAddress = new Uri("https://www.alphavantage.co/");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync($"query?function=GLOBAL_QUOTE&symbol={input}&apikey=1IB76KJ1VBK8IQXF");
-                try
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        JObject jsonResponse = await response.Content.ReadAsAsync<JObject>();
-
-                        Stock currStock = new Stock
-                        {
-                            Price = Double.Parse(jsonResponse["Global Quote"]["05. price"].ToString()),
-                            Name = jsonResponse["Global Quote"]["01. symbol"].ToString()
-                        };
-                        return currStock;
-                    }
-
-                    else
-                    {
-                        Stock failedStock = new Stock();
-                        failedStock.Name = "error";
-                        failedStock.Price = 0;
-                        return failedStock;
-                    }
-                }
-                catch
-                {
-                    Stock failedStock = new Stock();
-                    failedStock.Name = "error";
-                    failedStock.Price = 0;
-                    return failedStock;
-                }
-            }
+              Price = Double.Parse(jsonResponse["Global Quote"]["05. price"].ToString()),
+              Name = jsonResponse["Global Quote"]["01. symbol"].ToString()
+            };
+            return currStock;
+          }
+          else
+          {
+            Stock failedStock = new Stock();
+//will give more meaningful error in the future
+            failedStock.Name = "error";
+            failedStock.Price = 0;
+            return failedStock;
+          }
         }
+        catch
+        {
+          Stock failedStock = new Stock();
+//will give more meaningful error in the future
+          failedStock.Name = "error";
+          failedStock.Price = 0;
+          return failedStock;
+        }
+      }
     }
+  }
 }
