63727d2db9060208481640535c65aa9337194543
Kris Clancy <49080445+KrisPC@users.noreply.github.com>
2020-04-13 14:11:35 -0500

    new HW
    sprint 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,21 +30,26 @@ namespace StockProgram
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(280, 191);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 34);
             this.button1.TabIndex = 0;
@@ -54,12 +59,16 @@ namespace StockProgram
             // 
             // textBox1
             // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(108, 123);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.MaxLength = 4;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(161, 26);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label3
@@ -107,16 +116,15 @@ namespace StockProgram
             this.textBox3.TabIndex = 7;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox4
+            // textBox2
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(91, 301);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(178, 101);
-            this.textBox4.TabIndex = 8;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(91, 301);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 101);
+            this.textBox2.TabIndex = 8;
             // 
             // label5
             // 
@@ -129,22 +137,57 @@ namespace StockProgram
             this.label5.TabIndex = 9;
             this.label5.Text = "Result: ";
             // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(280, 301);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 34);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "SAVE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(280, 368);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 34);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "CLEAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(379, 424);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Stock App";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,8 +201,11 @@ namespace StockProgram
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,42 +1,107 @@
-﻿//Author:Kris, Noah, David
-// date 03/11/2020
-// objective: proof of concept
-//API used alphavatage
+﻿///Author:Kris, Noah, David
+// date 04/13/2020
+// objective: Sprint 2
+//API used: alphavantage
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace StockProgram
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            this.ActiveControl = textBox1;
         }
         private void label1_Click(object sender, EventArgs e)
         {
         }
+
+        public double total;
+
         private async void button1_Click(object sender, EventArgs e)
         {
-//take in a stock(s) and using the API find the stock and its
-// current value display this to the screen.
-            string input = textBox1.Text;
-            Stock s = await Program.RunASync(input);
-            textBox2.Text = "Name of stock: " + s.Name + Environment.NewLine + "Price of Stock: " + s.Price;
+            try
+            {
+                //take in a stock(s) and using the API find the stock and its
+                // current value display this to the screen. 
+                string input = textBox1.Text;
+                string input2 = textBox3.Text;
+                double inputDouble = Convert.ToDouble(input2);
+                Stock s = await Program.RunASync(input);
+                double answer = (s.Price * inputDouble);
+                total += answer;
+                textBox2.Text = textBox3.Text + " shares of " + s.Name + " at price of " + s.Price + " is equal to " + answer + Environment.NewLine + "Total is: " + total;
+                
+
+            }
+                
+            catch
+            {
+                textBox2.Text = "ERROR, CHECK INPUT BOXES";
+
+            }
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+
         }
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
         }
+
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(textBox2.Text + Environment.NewLine + "FILE CREATED SUCCESSFULLY", "Total Box");
+            string messageDate = (DateTime.Now.ToString("F"));
+
+            string path = Environment.CurrentDirectory + "/" + "History.txt";
+
+            List<string> lines = File.ReadAllLines(path).ToList();
+            lines.Add("---------");
+            lines.Add(textBox2.Text);
+            lines.Add(messageDate);
+            File.ReadAllLines(path);
+
+            //We only want one statement for if the file doesn't already exist in the directory
+            if (!File.Exists(path))
+            {
+                File.CreateText(path);
+
+            }
+
+            //We want whatever text that we collected from 'saving' our data in the app
+            File.WriteAllLines(path, lines);
+            Console.ReadLine();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            total = 0;
+            textBox1.Text = "";
+            textBox3.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
         {
 
         }
